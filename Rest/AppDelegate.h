ed48828a6934e51700cd8592b4e9a4c13fe7056f
Amit Gawai <amit.gawai789@icloud.com>
2018-01-04 16:22:14 +0530

    Initial Commit

---
+++ Rest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Rest
+//
+//  Created by Amit Gawai on 04/01/18.
+//  Copyright © 2018 Amit Gawai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Rest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Rest
+//
+//  Created by Amit Gawai on 04/01/18.
+//  Copyright © 2018 Amit Gawai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
